f5d393488c1f7427cfcc9433430df33a876204f9
Yago <yagodev10@gmail.com>
2026-04-19 00:50:53 -0300

    Conexão com o DBA (SupaBase) + .env

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,14 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:WxU65wCVSlZSiRNO@db.tieyyoatvstjmzubkrzo.supabase.co:5432/postgres"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("⚠️ ERRO: A variável DATABASE_URL não foi encontrada no arquivo .env!")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Função para obter o DB nas rotas ou scripts
 def get_db():
     db = SessionLocal()
     try:
